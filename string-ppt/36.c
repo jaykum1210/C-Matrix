9d049edc5eda2faf09e418ecd32354a23cb25bcd
jay kumawat <jaykumawat1210@gmail.com>
2025-09-13 19:28:38 +0530

    string-ppt

--- string-ppt/36.c
+++ string-ppt/36.c
@@ -2,28 +2,28 @@
 #include <string.h>
 
 int main(){
-    char a[100],b[50],max[100];
-    printf("Enter string = ");
+    char a[100],temp[100],max[100];
+    printf("Enter largest string = ");
     fgets(a,sizeof(a),stdin);
-    int c = strlen(a);
-    int j = 0, maxlen = 0;
-    for (int i = 0; i < c; i++)
+    int b = strlen(a);
+    int i,j=0,maxlen = 0;
+    for (int i = 0; i < b; i++)
     {
-        if (a[i] != ' ' && a[i]!= '\0' && a[i]!= '\n')
+        if (a[i] != ' ' && a[i] != '\0' && a[i] != '\n')
         {
-            b[j] = a[i];
+            temp[j] = a[i];
             j++;
         }
         else{
-            b[j] = '\0';
+            temp[j] = '\0';
             if (j>maxlen)
             {
                 maxlen = j;
-                strcpy(max,b);
+                strcpy(max,temp);
             }
             j = 0;
         }
     }
-    printf("Largest character = %s\n",max);
+    printf("Largest String = %s\n",max);
     return 0;
 }
--- string-ppt/37.c
+++ string-ppt/37.c
@@ -2,20 +2,28 @@
 #include <string.h>
 
 int main(){
-    char a[100],b[100],min[50];
-    printf("Enter String = ");
+    char a[100],temp[100],min[100];
+    printf("Enter string = ");
     fgets(a,sizeof(a),stdin);
-    int j = 0,minlen = 0;
-    int c = strlen(a);
-    for (int i = 0; i < a; i++)
+    int b = strlen(a);
+    int i,j=0,minlen=100;
+    for (int i = 0; i < b; i++)
     {
-        if (a[i]==' ' || a[i]=='\0' || a[i]=='\n')
+        if (a[i] != ' ' && a[i] != '\0' && a[i] != '\n')
         {
-            b[j++]=a[i];
+            temp[j] = a[i];
+            j++;
         }
         else{
-            
+            temp[j] = '\0';
+            if (minlen>j)
+            {
+                minlen = j;
+                strcpy(min,temp);
+            }
+            j=0;
         }
     }
-    
+    printf("Smallest string = %s\n",min);
+    return 0;
 }
