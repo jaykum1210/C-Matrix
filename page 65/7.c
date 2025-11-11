9f71fe00136e982b2ab84f4b6708e12474f300d3
jaykum1210 <jaykumawat1210@gmail.com>
2025-11-11 20:13:56 +0530

    add files

--- page 65/7.c
+++ page 65/7.c
@@ -5,29 +5,29 @@ int main(){
     char a[100],ch;
     printf("Enter string = ");
     fgets(a,sizeof(a),stdin);
-    int freq = 0,num=0;
     int b = strlen(a);
     if (a[b-1]=='\n')
     {
         a[b-1] = '\0';
         b--;
     }
-    
+    int max=0,num;
     for (int i = 0; i < b; i++)
     {
         num=0;
         for (int j = i+1; j < b; j++)
         {
-            if (a[i]==a[j])
+            if (a[i]==a[j] && a[i]!=' ')
             {
                 num++;
             }
         }
-        if (num>freq)
+        if (num>max)
         {
+            max = num;
             ch = a[i];
         }
     }
-    printf("\'%c\' occurs %d times\n",ch,num);
+    printf("Maximum ocuurance is of letter \'%c\'",ch);
     return 0;
 }
