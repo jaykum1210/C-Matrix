1b27ef61884e406115f821e666ab41b34b8c5801
jaykum1210 <jaykumawat1210@gmail.com>
2025-11-18 12:03:37 +0530

    add files

---
+++ page 68/1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    char a[100],b[100];
+    printf("Enter string = ");
+    fgets(a,sizeof(a),stdin);
+    int c = strlen(a);
+    if (a[c-1]=='\n')
+    {
+        a[c-1]='\0';
+        c--;
+    }
+    for (int i = c-1; i>=0; i--)
+    {
+        b[c-i-1] = a[i];
+    }
+    b[c] = '\0';
+    printf("String = %s",b);
+    return 0;
+}
