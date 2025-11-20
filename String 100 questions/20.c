37ee7755c74f7ae5be09da3bfcecff8413f1d245
jaykum1210 <jaykumawat1210@gmail.com>
2025-11-20 19:23:08 +0530

    add files

--- String 100 questions/20.c
+++ String 100 questions/20.c
@@ -3,5 +3,45 @@
 
 int main(){
     char a[100],ch;
+    printf("Enter string = ");
+    fgets(a,sizeof(a),stdin);
+    int len = strlen(a);
+    if (a[len-1]=='\n')
+    {
+        a[len-1] = '\0';
+        len--;
+    }
     
+    int min = __INT_MAX__;
+    int num = 0;
+    for (int i = 0; i < len; i++)
+    {
+        int found = 0;
+        num = 0;
+        for (int j = i-1; j >=0; j--)
+        {
+            if (a[i]==a[j])
+            {
+                found=1;
+                break;
+            }
+        }
+        if (found==0)
+        {
+            for (int j = 0; j <=len; j++)
+            {
+                if (a[i]==a[j])
+                {
+                    num++;
+                }
+            }
+            if (min>num)
+            {
+                min = num;
+                ch = a[i];
+            }
+        }
+    }
+    printf("\'%c\' occurce %d times",ch,min);
+    return 0;
 }
