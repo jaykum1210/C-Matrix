e7e7c6f0fc0db0807deb64fbc67fd908373f965e
jaykum1210 <jaykumawat1210@gmail.com>
2025-11-20 19:26:44 +0530

    add files

---
+++ String 100 questions/21.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    char a[100];
+    printf("Enter string = ");
+    fgets(a,sizeof(a),stdin);
+    int len = strlen(a);
+    if (a[len-1]=='\n')
+    {
+        a[len-1]='\0';
+        len--;
+    }
+    for (int i = 0; i < len; i++)
+    {
+        printf("%d ",a[i]);
+    }
+    return 0;
+}
