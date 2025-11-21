841ec58db69137021b30e77e9c079e48c03e5e6d
jaykum1210 <jaykumawat1210@gmail.com>
2025-11-21 13:43:37 +0530

    add files

---
+++ String 100 questions/32.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    char a[100];
+    printf("Enter string = ");
+    fgets(a,sizeof(a),stdin);
+    int len = strlen(a);
+    if (a[len-1]=='\n')
+    {
+        a[len-1]='\0';
+        len--;
+    }
+    a[len-1]='\0';
+    printf("String = %s",a);
+    return 0;
+}
