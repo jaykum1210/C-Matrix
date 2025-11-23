7fa14de44354f74b8d256580056c2bfb1be47c62
jaykum1210 <jaykumawat1210@gmail.com>
2025-11-23 22:43:10 +0530

    add files

---
+++ function ppt/22-1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    char a[100];
+    printf("Enter string = ");
+    fgets(a,sizeof(a),stdin);
+    strupr(a);
+    printf("String = %s",a);
+    return 0;
+}
