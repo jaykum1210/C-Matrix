01432ffc582db231dcae68f598825ebba6309781
jaykum1210 <jaykumawat1210@gmail.com>
2025-11-23 18:39:22 +0530

    add files

--- function ppt/14.c
+++ function ppt/14.c
@@ -1,1 +1,18 @@
 #include <stdio.h>
+
+int sum(int a){
+    if (a==0)
+    {
+        return 0;
+    }
+    return (a%10) + sum(a/10);
+}
+
+int main(){
+    int a;
+    printf("Enter number = ");
+    scanf("%d",&a);
+    int b = sum(a);
+    printf("Sum of digits = %d\n",b);
+    return 0;
+}
