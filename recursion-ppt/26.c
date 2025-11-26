94d6faceff0a8a427463851607f62615262512cc
jaykum1210 <jaykumawat1210@gmail.com>
2025-11-26 23:25:01 +0530

    add files

--- recursion-ppt/26.c
+++ recursion-ppt/26.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 
+int lcm(int min, int max, int i){
+    if ((max*i)%min==0)
+    {
+        return max*i;
+    }
+    return lcm(min,max,i+1);
+}
+
 int main(){
     int a,b;
-    printf("Enter two numbers = ")
+    printf("Enter two numbers = ");
+    scanf("%d%d",&a,&b);
+    int max = a;
+    int min = b;
+    if (b>max)
+    {
+        max = b;
+        min = a;
+    }
+    int i = 1;
+    int num = lcm(min,max,i);
+    printf("LCM of %d and %d = %d\n",a,b,num);
+    return 0;
 }
