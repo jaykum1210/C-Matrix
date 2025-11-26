2a1c18653c18b4067c55fde4204148b09aac1cd8
jaykum1210 <jaykumawat1210@gmail.com>
2025-11-26 14:14:35 +0530

    add files

---
+++ recursion-ppt/10.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int rev(int a){
+    static int r = 0;
+    if (a==0)
+    {
+        return r;
+    }
+    r = r*10 + (a%10);
+    return rev(a/10);
+}
+
+int main(){
+    int a;
+    printf("Enter number = ");
+    scanf("%d",&a);
+    int b = rev(a);
+    if (a==b)
+    {
+        printf("Palindrome\n");
+    }
+    else{
+        printf("Not Palindrome\n");
+    }
+    return 0;
+}
