12ffd7757135cac412d912a9b72d758de01c79d0
jaykum1210 <jaykumawat1210@gmail.com>
2025-11-26 23:06:56 +0530

    add files

--- recursion-ppt/25.c
+++ recursion-ppt/25.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-int gcd(int a, int b){
-    if (a==0)
+int gcd(int a,int b){
+    if (b==0)
     {
-        return b;
+        return a;
     }
     return gcd(b,a%b);
 }
@@ -12,7 +12,7 @@ int main(){
     int a,b;
     printf("Enter two numbers = ");
     scanf("%d%d",&a,&b);
-    int num = gcd(a,b);
-    printf("GCD of %d and %d = %d\n",a,b,num);
+    int c = gcd(a,b);
+    printf("GCD of %d and %d = %d\n",a,b,c);
     return 0;
 }
---
+++ recursion-ppt/26.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(){
+    int a,b;
+    printf("Enter two numbers = ")
+}
