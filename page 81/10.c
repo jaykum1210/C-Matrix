4a56e4ef59df4f3977a643cdbd99d3a1babd6448
jaykum1210 <jaykumawat1210@gmail.com>
2025-11-27 14:33:00 +0530

    add files

---
+++ page 81/10.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main(){
+    char a;
+    printf("Enter character = ");
+    scanf("%c",&a);
+    if (isalpha(a))
+    {
+        printf("1\n");
+    }
+    else{
+        printf("0\n");
+    }
+    return 0;
+}
