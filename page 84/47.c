399a2e40283bbe4e7f21f16671ab110714f00547
jaykum1210 <jaykumawat1210@gmail.com>
2025-11-30 22:48:40 +0530

    add files

---
+++ page 84/47.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <string.h>
+
+int rec(char a[], char b[], int len1, int len2){
+    if (len1>len2)
+    {
+        return -1;
+    }
+    else if (len2>len1)
+    {
+        return 1;
+    }
+    for (int i = 0; i < len1; i++)
+    {
+        if (a[i]>b[i])
+        {
+            return -1;
+        }
+        else if (a[i]<b[i])
+        {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int main(){
+    char a[100],b[100];
+    printf("Enter 1st string = ");
+    fgets(a,sizeof(a),stdin);
+    printf("Enter 2nd string = ");
+    fgets(b,sizeof(b),stdin);
+    int len1 = strlen(a);
+    if (a[len1-1]=='\n')
+    {
+        a[len1-1]='\0';
+        len1--;
+    }
+    int len2 = strlen(b);
+    if (b[len2-1]=='\n')
+    {
+        b[len2-1]='\0';
+        len2--;
+    }
+    int num = rec(a,b,len1,len2);
+    if (num==1)
+    {
+        printf("%s is bigger\n",b);
+    }
+    else if (num==-1)
+    {
+        printf("%s is bigger\n",a);
+    }
+    else{
+        printf("Both are Lexixographically\n");
+    }
+    return 0;
+}
