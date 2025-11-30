d2ea8a29462f51a86148c75f3f06460ab7222cff
jaykum1210 <jaykumawat1210@gmail.com>
2025-11-30 11:04:55 +0530

    add files

---
+++ page 84/33-b.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+void rev(char a[], int len){
+    for (int i = 0; i < len/2; i++)
+    {
+        char temp = a[i];
+        a[i] = a[len-i-1];
+        a[len-i-1] = temp;
+    }
+}
+
+int main(){
+    char a[100];
+    printf("Enter string = ");
+    fgets(a,sizeof(a),stdin);
+    int len = strlen(a);
+    if (a[len-1]=='\n')
+    {
+        a[len-1]='\0';
+        len--;
+    }
+    rev(a,len);
+    printf("String = %s",a);
+    return 0;
+}
