c3f0656252b90168d1bf2c7794b09a9995e4d383
jaykum1210 <jaykumawat1210@gmail.com>
2025-12-04 21:14:15 +0530

    add files

---
+++ Simple pointer/21.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    int a;
+    printf("Enter size = ");
+    scanf("%d",&a);
+    int b[a];
+    printf("Enter %d elements = ",a);
+    for (int i = 0; i < a; i++)
+    {
+        scanf("%d",&b[i]);
+    }
+    int *p = b;
+    printf("First element = %d\n",*p);
+    return 0;
+}
