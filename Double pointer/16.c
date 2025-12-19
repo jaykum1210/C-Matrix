f9d5d2bf25d120c5bc1e3fcfae7ca11ac06ab1eb
jaykum1210 <jaykumawat1210@gmail.com>
2025-12-19 09:24:32 +0530

    add files

--- Double pointer/16.c
+++ Double pointer/16.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+int *fun(int **pp){
+    return *pp;
+}
+
 int main(){
-    
+    int a = 10;
+    int *p = &a;
+    int **pp = &p;
+    int *val = fun(pp);
+    printf("Value = %d\n",*val);
+    return 0;
 }
