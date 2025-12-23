f178499c64bd56d2ca13002d989284d253503448
jaykum1210 <jaykumawat1210@gmail.com>
2025-12-23 20:57:37 +0530

    add file

--- Double pointer/38.c
+++ Double pointer/38.c
@@ -4,7 +4,7 @@ int main(){
     int a = 10;
     int *p = &a;
     int **pp = &p;
-    printf("Value = %d\n",*pp);
+    printf("Value = %p\n",*pp);
     printf("Address = %p\n",&p);
     return 0;
 }
