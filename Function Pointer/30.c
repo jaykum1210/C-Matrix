808f9c4260a44cea9314d98cd7c8bfd75ffef830
jaykum1210 <jaykumawat1210@gmail.com>
2025-12-25 15:29:45 +0530

    add files

---
+++ Function Pointer/30.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+void sort(int b[], int a){
+    for (int i = 0; i < a-1; i++)
+    {
+        for (int j = 0; j < a-i-1; j++)
+        {
+            if (b[j]>b[j+1])
+            {
+                int temp = b[j];
+                b[j] = b[j+1];
+                b[j+1]=temp;
+            }
+        }
+    }
+}
+
+int main(){
+    int a;
+    printf("Enter size = ");
+    scanf("%d",&a);
+    int b[a];
+    printf("Enter %d elements = ",a);
+    for (int i = 0; i < a; i++)
+    {
+        scanf("%d",&b[i]);
+    }
+    void (*fp)(int *, int);
+    fp = sort;
+    fp(b, a);
+    printf("Array = ");
+    for (int i = 0; i < a; i++)
+    {
+        printf("%d ",b[i]);
+    }
+    return 0;
+}
