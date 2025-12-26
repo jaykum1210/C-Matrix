f1a74bcf4b487f578c1501c84569606a506a1a05
jaykum1210 <jaykumawat1210@gmail.com>
2025-12-26 19:07:35 +0530

    add files

--- Function Pointer/49.c
+++ Function Pointer/49.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
 
-/* Function for first partial value */
 int part1(int a) {
     return a;
 }
 
-/* Function for second partial value */
 int part2(int b) {
     return b;
 }
 
-/* Function for final sum */
 int total(int x, int y) {
     return x + y;
 }
@@ -21,14 +18,14 @@ int main() {
     printf("Enter two numbers = ");
     scanf("%d %d", &a, &b);
 
-    int (*fp1)(int) = part1;        // function pointer 1
-    int (*fp2)(int) = part2;        // function pointer 2
-    int (*fp3)(int, int) = total;   // function pointer 3
+    int (*fp1)(int) = part1;
+    int (*fp2)(int) = part2;
+    int (*fp3)(int, int) = total;
 
-    int p1 = fp1(a);    // partial sum 1
-    int p2 = fp2(b);    // partial sum 2
+    int p1 = fp1(a);
+    int p2 = fp2(b);
 
-    int sum = fp3(p1, p2);   // final sum
+    int sum = fp3(p1, p2);
 
     printf("Final Sum = %d\n", sum);
 
