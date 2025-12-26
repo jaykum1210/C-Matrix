895d52a0fe324b7e45e6b7ef19a6b4feaacc631d
jaykum1210 <jaykumawat1210@gmail.com>
2025-12-26 19:37:59 +0530

    add files

---
+++ pointer to array/7.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    int a;
+    printf("Enter size = ");
+    scanf("%d",&a);
+    int b[a];
+    printf("Enter %d elements = ",a);
+    for (int i = 0; i < a; i++)
+    {
+        scanf("%d",&b[i]);
+    }
+    int (*p)[a];
+    p = &b;
+    printf("Last element = %d\n",(*p)[a-1]);
+    return 0;
+}
