cb4ee56d3e05ebf695cfd1447e131d9973745b60
jaykum1210 <jaykumawat1210@gmail.com>
2025-12-28 21:10:27 +0530

    add files

---
+++ Array of pointer/4.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+    int a;
+    printf("Enter size = ");
+    scanf("%d",&a);
+    int b[a];
+    int *arr[a];
+    printf("Enter %d elements = ",a);
+    for (int i = 0; i < a; i++)
+    {
+        scanf("%d",&b[i]);
+        arr[i] = &b[i];
+    }
+    printf("Array = ");
+    for (int i = 0; i < a; i++)
+    {
+        printf("%d ",*arr[i]);
+    }
+    return 0;
+}
