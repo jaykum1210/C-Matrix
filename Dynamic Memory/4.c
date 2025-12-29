6183c280ac0e85f757d80309b9270e8cea02d433
jaykum1210 <jaykumawat1210@gmail.com>
2025-12-29 22:36:47 +0530

    add files

---
+++ Dynamic Memory/4.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int a;
+    printf("Enter size = ");
+    scanf("%d",&a);
+    int *p;
+    p = (int *)malloc(a*sizeof(int));
+    printf("Enter %d elements = ",a);
+    for (int i = 0; i < a; i++)
+    {
+        scanf("%d",&p[i]);
+    }
+    printf("Array = ");
+    for (int i = 0; i < a; i++)
+    {
+        printf("%d ",p[i]);
+    }
+    return 0;
+}
