ace954dbfb69d3ad7114e9612ddbbb27efd9c2f5
jaykum1210 <jaykumawat1210@gmail.com>
2025-12-30 19:32:26 +0530

    add files

---
+++ Dynamic Memory/41.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int a;
+    printf("Enter size = ");
+    scanf("%d",&a);
+    int *p;
+    p = (int *)malloc(a*sizeof(int));
+    if (p==NULL)
+    {
+        printf("Memory Allocation Failed\n");
+        return 0;
+    }
+    printf("Enter %d elements = ",a);
+    for (int i = 0; i < a; i++)
+    {
+        scanf("%d",&p[i]);
+    }
+    int val;
+    printf("Enter new size = ");
+    scanf("%d",&val);
+    p = (int *)realloc(p,val*sizeof(int));
+    if (p==NULL)
+    {
+        printf("Memory Allocatio Failed\n");
+        return 0;
+    }
+    printf("Array after reallocation = ");
+    for (int i = 0; i < val; i++)
+    {
+        printf("%d ",p[i]);
+    }
+    free(p);
+    return 0;
+}
