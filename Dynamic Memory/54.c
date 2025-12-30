99ffa336fdb3528da274872a8d23f7445b50eb48
jaykum1210 <jaykumawat1210@gmail.com>
2025-12-30 23:29:42 +0530

    add files

---
+++ Dynamic Memory/54.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int a;
+    printf("Enter size = ");
+    scanf("%d",&a);
+    int *p;
+    p = (int *)malloc(a*sizeof(int));
+    if (p==NULL)
+    {
+        printf("Memory Allocation Failed");
+        return 0;
+    }
+    printf("Enter %d elements = ",a);
+    for (int i = 0; i < a; i++)
+    {
+        scanf("%d",(p+i));
+    }
+    int count = 0;
+    for (int i = 0; i < a; i++)
+    {
+        if (p[i]<0)
+        {
+            count++;
+        }
+    }
+    printf("Negative numbers = %d\n",count);
+    free(p);
+    return 0;
+}
