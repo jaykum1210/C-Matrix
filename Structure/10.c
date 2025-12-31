568c19c46ad042e814852e38a445327185a8c600
jaykum1210 <jaykumawat1210@gmail.com>
2025-12-31 18:40:27 +0530

    add files

--- Structure/10.c
+++ Structure/10.c
@@ -1,3 +1,13 @@
 #include <stdio.h>
 
-struct 
+struct number {
+    int a;
+};
+
+int main(){
+    struct number n1,n2;
+    printf("Enter two numbers = ");
+    scanf("%d%d",&n1.a,&n2.a);
+    printf("Number = %d %d\n",n1.a,n2.a);
+    return 0;
+}
